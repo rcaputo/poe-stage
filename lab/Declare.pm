4297848833e8fa4acecdc504a4ad9cb1a8075b2c
Rocco Caputo <rcaputo@cpan.org>
2006-10-29 08:11:22 +0000

    Punt.  A single declarator cannot handle scalar initialization and nonscalar declaration simultaneously.  Write two instead: init() to handle scalar initialization, and declare() that permits declaration of multiple variable types.  Later if it's determined Perl can handle both with one function, we can combine the implementations and make one an alias for the other.

--- lab/Declare.pm
+++ lab/Declare.pm
@@ -2,37 +2,86 @@
 
 # Support
 #
-#   declare my $scalar = $value;
+#   init my $scalar = $value;
 #   declare my $scalar;
-#   declare my ($scalar, $scalar) = ($val1, $val2);
+#   init my ($scalar, $scalar) = ($val1, $val2);
 #   declare my ($a, $b), my($c, $d);
+#   declare my @a, my $b, my %c;
 #   ...
 #
 # See declare.perl for sample applications.
 #
-# CAVEAT: Only scalars may be declared.  I'm not sure how to declare
-# arrays since the members of declare()'s @_ are aliases to the values
-# in the array, not the variable being declared.  var_name() therefore
-# can't find the variable name.
+# Summary: There are two declarators, declare() and init().  Declare()
+# can declare nonscalar types, but you can't initialize them.  init()
+# can initialize scalars, but you can't declare nonscalars.
+#
+# Why one function can't do both:  The (@) type flattens its
+# parameters, so @_ within the called function contains elements from
+# arrays and hashes passed to the function.  The function can't
+# determine with any accuracy what the original variables were.  Matt
+# Trout suggested Devel::Caller, but as of this time it can't handle
+# more complex declarations.
+#
+# Meanwhile the (\[$@%]) prototype can't handle assignments.  You
+# can't say "declare my $x = 1" because it throws an error about
+# calling declare() with an assignment.
+#
+# The current compromise is to implement two forms of declarator:
+# declare() that handles any variable type but no initialization, and
+# init() that allows initialization, but only for scalars.  It's not
+# perfect, but at least we get two functions with reasonably
+# understandable names.
 
 package Declare;
 
 use warnings;
 use strict;
 
-use PadWalker qw(var_name);
+use PadWalker qw(var_name peek_our);
 use Exporter;
 use base qw(Exporter);
-our @EXPORT = qw(declare declare_a declare_u);
+our @EXPORT = qw(init declare);
 
-sub declare (@) {
-	print "declare() called with:\n";
+sub init (@) {
+	print "init() called with:\n";
 	for (my $i = 0; $i < @_; $i++) {
-		my $val = $_[$i];
-		$val = "(undef)" unless defined $val;
-		$val = "(empty)" unless length $val;
+		my $var_reference = \$_[$i];
 
-		print "  var $i (", \$_[$i], "): ", var_name(1, \$_[$i]), " = $val\n";
+		my $var_value;
+		if (ref($var_reference) eq "SCALAR") {
+			$var_value = $$var_reference;
+		}
+		elsif (ref($var_reference) eq "ARRAY") {
+			$var_value = "@$var_reference";
+		}
+		elsif (ref($var_reference) eq "HASH") {
+			$var_value = (
+				join "; ",
+				map { "$_ = $var_reference->{$_}" }
+				keys %$var_reference
+			);
+		}
+
+		$var_value = "(undef)" unless defined $var_value;
+		$var_value = "(empty)" unless length $var_value;
+
+		# Try lexicals first.
+
+		my $var_name = var_name(1, $var_reference);
+
+		# Try globals.  This is from Philip Gwyn's declare_a().
+		# This is an interesting variant of declare().  Is it useful?
+
+		unless ($var_name) {
+			my $vars = peek_our( 1 );
+			while( my( $name, $reference ) = each %$vars ) {
+				next unless $reference == $var_reference;
+				$var_name = $name;
+				last;
+			}
+		}
+
+		print "  var $i ($var_reference): $var_name = $var_value\n";
 
 		# The intended application:
 		#   Remove the sigil, and hang onto it.
@@ -43,69 +92,59 @@ sub declare (@) {
 	}
 }
 
-# Philip Gwyn's array declaration.
-#
-# This works, with one simple but (to me) annoying caveat: Unlike
-# my() and declare(), declare_a() only works on a single variable at a
-# time.  These are illegal:
-#
-#   declare( my (@a, @b );
-#   declare my @a, my @b;
-#   declare my $scalar, my @array;
-#
-# We also can't say this because the first argument to declare_a()
-# becomes a "list assignment":
-#
-#   declare my @array = qw(list1 list2 list3);
-
-sub declare_a (\@) {
-	print "declare_a() called:\n";
-
-	# Try lexicals first.
-
-	my $var_name = var_name(1, $_[0]);
-
-	# Try globals.
-	# This is an interesting variant of declare().  Is it useful?
-
-	unless ($var_name) {
-		my $vars = peek_our( 1 );
-		while( my( $name, $reference ) = each %$vars ) {
-			next unless $reference == $_[0];
-			$var_name = $name;
-			last;
-		}
-	}
-
-	print "  var 0 $var_name = (@{$_[0]})\n";
-}
-
 # Matt Trout suggested the \[$@%] prototype syntax, which I had never
-# heard of before.  But it's documented.  Here's an attempt at it.
+# heard of before.  But it's documented, so cool.
 # FIXME - I wish there were a "varargs" modifier to prototypes, to say
 # "and zero or more of this".  Until there is one, extend the
 # prototype as far as necessary.
 
-sub declare_u (\[$@%];\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]) {
-	print "declare_u() called with:\n";
+sub declare (\[$@%];\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%]\[$@%\[$@%\[$@%]]]\[$@%]) {
+	print "init() called with:\n";
 	for (my $i = 0; $i < @_; $i++) {
-		my $ref = $_[$i];
+		my $var_reference = $_[$i];
 
-		my $val;
-		if (ref($_[$i] eq "SCALAR")) {
-			$val = $$ref;
+		my $var_value;
+		if (ref($var_reference) eq "SCALAR") {
+			$var_value = $$var_reference;
 		}
-		elsif (ref($_[$i] eq "ARRAY")) {
-			$val = "@$ref";
+		elsif (ref($var_reference) eq "ARRAY") {
+			$var_value = "@$var_reference";
 		}
-		elsif (ref($_[$i] eq "HASH")) {
-			$val = join "; ", map { "$_ = $ref->{$_}" } keys %$ref;
+		elsif (ref($var_reference) eq "HASH") {
+			$var_value = (
+				join "; ",
+				map { "$_ = $var_reference->{$_}" }
+				keys %$var_reference
+			);
 		}
 
-		$val = "(undef)" unless defined $val;
-		$val = "(empty)" unless length $val;
+		$var_value = "(undef)" unless defined $var_value;
+		$var_value = "(empty)" unless length $var_value;
 
-		print "  var $i (", $ref, "): ", var_name(1, $ref), " = $val\n";
+		# Try lexicals first.
+
+		my $var_name = var_name(1, $var_reference);
+
+		# Try globals.  This is from Philip Gwyn's declare_a().
+		# This is an interesting variant of declare().  Is it useful?
+
+		unless ($var_name) {
+			my $vars = peek_our( 1 );
+			while( my( $name, $reference ) = each %$vars ) {
+				next unless $reference == $var_reference;
+				$var_name = $name;
+				last;
+			}
+		}
+
+		print "  var $i ($var_reference): $var_name = $var_value\n";
+
+		# The intended application:
+		#   Remove the sigil, and hang onto it.
+		#   If the variable matches /^arg_/, then do the :Arg magic.
+		#   If it matches /^req_/, then do :Req magic.
+		#   If it's "$" . "self", then do :Self magic.
+		#   etc.
 	}
 }
 
--- lab/declare.perl
+++ lab/declare.perl
@@ -5,24 +5,54 @@
 # You can't have lexical names overlap between different contexts.
 # For example, my $something :Req = my $something :Arg.  Can't be
 # done!  Have I mentioned I hate it?
+
+# So, this is an attempt to find something better.  Define a grammar
+# for declaring variables in odd scopes:
 #
-# So, this is an attempt to find something better.  Define a "declare"
-# syntax that can grab the variable names and do things based on their
-# prefix.  The practical (I hope) use case would be:
+# declare($x, @y, %z) allows variables to be declared within
+# POE::Stage's various scopes.
 #
-#   declare(
-#     my $arg_something,
-#     my $req_something,
-#   );
+# init my $x = 1; allows declaration and initialization, but due to
+# Perl's syntactic limitations they may only be scalars.
+
+# Current sample output:
 #
-#   $req_something = $arg_something;
-#
-# As you'll see after __END__, this is technically possible.
+# 1) poerbook:~/projects/poe-stage/lab% perl declare.perl
+# init() called with:
+#   var 0 (SCALAR(0x180a2e4)): $a_1 = a1
+#   var 1 (SCALAR(0x180a2f0)): $a_2 = a2
+#   var 2 (SCALAR(0x180a338)): $a_3 = a3
+#   var 3 (SCALAR(0x180a320)): $a_4 = a4
+# init() called with:
+#   var 0 (SCALAR(0x180a3f8)): $b_1 = (undef)
+#   var 1 (SCALAR(0x180a464)): $b_2 = (undef)
+# init() called with:
+#   var 0 (SCALAR(0x180a488)): $c_1 = c1
+#   var 1 (SCALAR(0x180a578)): $c_2 = c2
+# init() called with:
+#   var 0 (SCALAR(0x182c1b0)): $d_1 = d1
+#   var 1 (SCALAR(0x182c1c8)): $d_2 = d2
+# init() called with:
+#   var 0 (SCALAR(0x182c204)): $e_1 = e1
+# init() called with:
+#   var 0 (SCALAR(0x182c234)): $f_1 = f1
+#   var 1 (SCALAR(0x182c24c)): $f_2 = f2
+#   var 2 (SCALAR(0x182c288)): $f_3 = f3
+#   var 3 (SCALAR(0x182c2a0)): $f_4 = f4
+# init() called with:
+#   var 0 (ARRAY(0x182c300)): @g_1 = (empty)
+# init() called with:
+#   var 0 (ARRAY(0x182c354)): @g_2 = (empty)
+# init() called with:
+#   var 0 (SCALAR(0x182c360)): $scalar = (undef)
+#   var 1 (ARRAY(0x182c390)): @array = (empty)
+#   var 2 (HASH(0x182c3a8)): %hash = (empty)
+# 
 
 {
 	use Declare;
 
-	declare(
+	init(
 		my($a_1, $a_2) = qw(a1 a2),
 		my($a_3, $a_4) = qw(a3 a4),
 	);
@@ -32,75 +62,22 @@
 		my $b_2,
 	);
 
-	declare my ($c_1, $c_2) = qw(c1 c2);
+	init my ($c_1, $c_2) = qw(c1 c2);
 
-	declare(
+	init(
 		my $d_1 = "d1",
 		my $d_2 = "d2",
 	);
 
-	declare my $e_1 = "e1";
+	init my $e_1 = "e1";
 
-	declare
+	init
 		my ($f_1, $f_2) = qw(f1 f2),
 		my ($f_3, $f_4) = qw(f3 f4);
 
-	# CAVEAT: Only scalars may be declared.  I'm not sure how to declare
-	# arrays since the members of declare()'s @_ are aliases to the
-	# values in the array, not the variable being declared.  var_name()
-	# therefore can't find the variable name.  This is therefore
-	# impossible at the moment.  See the bad output at the end.
+	declare my @g_1;
+	@g_1 = qw(g1a g1b g1c);
 
-	declare my @g_1 = qw(g1a g1b g1c);
-
-	# Philip Gwyn suggested a declare_a(), but it has some drawbacks: It
-	# only works on a single array at a time.  Thess syntaxes aren't
-	# legal:
-	#
-	#   declare_a my @array_1, my @array_2;
-	#   declare_a my(@array_1, @array_2);
-	#   declare_a my $scalar, my @array;
-	#   declare_a my @array = qw(list1 list2 list3);
-
-	declare_a my @g_2;
-
-	# Matt Trout suggested \[$@%] prototype syntax, which declare_u()
-	# attempts to use.  The "U" is for "Ueber". :)
-	#
-	# Still can't do list assignments here.  This is illegal:
-	#
-	#   declare_u my @array = qw(list1 list2 list3);
-	#
-	# But the other syntaxes are good.
-
-	declare_u my $scalar, my @array, my %hash;
+	declare my @g_2;
+	declare my $scalar, my @array, my %hash;
 }
-
-__END__
-
-1) poerbook:~/projects/poe-stage/lab% perl declare.perl     
-declare() called with:
-  var 0 (SCALAR(0x180a2e4)): $a_1 = a1
-  var 1 (SCALAR(0x180a2f0)): $a_2 = a2
-  var 2 (SCALAR(0x180a338)): $a_3 = a3
-  var 3 (SCALAR(0x180a320)): $a_4 = a4
-declare() called with:
-  var 0 (SCALAR(0x180a410)): $b_1 = (undef)
-  var 1 (SCALAR(0x180a44c)): $b_2 = (undef)
-declare() called with:
-  var 0 (SCALAR(0x180a470)): $c_1 = c1
-  var 1 (SCALAR(0x180a488)): $c_2 = c2
-declare() called with:
-  var 0 (SCALAR(0x1820890)): $d_1 = d1
-  var 1 (SCALAR(0x18208a8)): $d_2 = d2
-declare() called with:
-  var 0 (SCALAR(0x18208e4)): $e_1 = e1
-declare() called with:
-  var 0 (SCALAR(0x1820914)): $f_1 = f1
-  var 1 (SCALAR(0x182092c)): $f_2 = f2
-  var 2 (SCALAR(0x1820968)): $f_3 = f3
-  var 3 (SCALAR(0x1820980)): $f_4 = f4
-declare() called with:
-  var 0 (SCALAR(0x1801380)):  = g1a
-  var 1 (SCALAR(0x1801434)):  = g1b
-  var 2 (SCALAR(0x1801524)):  = g1c
